b5f268777e9104fc6416abb530d006c153992917
tlecomte1 <lecomte.t@protonmail.com>
2025-12-19 14:36:37 +0100

    Add files via upload

---
+++ Projet/CodeTL/NewEvent_RapportTL.hpp
@@ -0,0 +1,60 @@
+//Ajouter les includes qui manque, les #ifndef #define, #endif
+#include <iostream>
+
+class Event_Rapport {
+public:
+    enum class EventPriority {
+        IMMEDIATE,
+        LATER
+    };
+
+    // Constructeur de la classe
+    Event_Rapport(int id_event_rapport,         
+        std::string recommendation,
+        EventPriority event_priority = EventPriority::IMMEDIATE,
+        std::vector<Security_Event*> associated_event = {},
+        std::tm time_stamp = {}):                               // LE TIME STAMP PEUT ETRE DE TYPE TM (time stamp) OU DE TYPE STRING. PERSO JE RECOMMENDE TIME_STAMP
+            Id_Event_Rapport(id_event_rapport),
+            Recommendation(recommendation),
+            Event_Priority(event_priority),
+            Associated_Event(associated_event),
+            Time_Stamp(time_stamp) {}
+
+    //Getters de la classe, a commenter selon les besoins
+    int getIdEventRapport() const { return Id_Event_Rapport; }
+    std::string getRecommendation() const { return Recommendation; }
+    EventPriority getEventPriority() const { return Event_Priority; }
+    std::vector<Security_Event*> getAssociated_Event() const { return Associated_Event; }
+    std::tm getTimeStamp() const { return Time_Stamp; }
+
+
+private:
+    int Id_Event_Rapport;
+    std::string Recommendation;
+    EventPriority Event_Priority;
+    std::vector<Security_Event*> Associated_Event;
+    std::tm Time_Stamp;
+
+//CREER LE PROTOTYPE ET LA METHODE POUR RECUPERER ET COMPILER LES SECURITY_EVENT DANS UN MEME RAPPORT
+    //CREER UN MODELE DE RAPPORT
+
+//CREER LE PROTOTYPE ET LA METHODE POUR EXPORTER LES RAPPORTS DANS LES ARCHIVES
+    //L'exportation doit etre possible en tant que telechargement vers un fichier 
+    // et l'exportation doit etre possible en direction des archives. Il faudra donc
+    // laisser la possibilité de choisir entre les deux lors de l'exportation. 
+
+//CREER UN PROTOTYPE ET SA METHODE POUR RENPLIR LE VECTEUR Associated_Event AVEC L'ID DES SECURITY_EVENT DE L'ATTAQUE
+
+//Créer une méthode pour saisir un texte de recommandation si on veut. 
+
+// Méthode statique pour créer un rapport
+    static Event_Rapport createRapport(
+        //int id_event_rapport,
+        //const std::vector<Security_Event>& security_events,
+        //const std::vector<Log>& logs,
+        //EventPriority priority = EventPriority::IMMEDIATE,
+        //std::tm time_stamp = {}
+    );
+    
+
+};
---
+++ Projet/CodeTL/NewSecurity_EventTL.hpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+class Security_Event {
+public:
+    enum class EventGravity {
+        HIGH,
+        MEDIUM,
+        LOW
+    };
+
+    // Constructeur 
+    Security_Event(int id_security_event,
+        std::string description,                    
+        EventGravity event_gravity = EventGravity::HIGH,
+        std::vector<Logs*> associated_logs = {},
+        std::tm time_stamp = {}): 
+            Id_Security_Event(id_security_event),
+            Description(description),
+            Event_Gravity(event_gravity),
+            Associated_Logs(associated_logs),
+            Time_Stamp(time_stamp) {}
+
+    // Getters
+    //Getters a commenter selon les besoins (utilisé pour creer le Event_Rapport)
+    int getIdSecurityEvent() const { return Id_Security_Event; }
+    std::string getDescription() const { return Description; }
+    EventGravity getEventGravity() const { return Event_Gravity; }
+    std::vector<Logs*> getAssociatedLogs() const { return Associated_Logs; }
+    std::tm getTimeStamp() const { return Time_Stamp; }
+
+
+    //CREER UN PROTOTYPE ET SA METHODE POUR DETECTER UNE ATTAQUE = THOMAS S'OCCUPE DE L'ALGO DE DETECTION
+        //IL FAUT QUE LES INFOS DES GETTER UTILISÉ DANS L'ALGO DE DETECTION PUISSENT ETRE UTILISÉE POUR REMPLIR LE RAPPORT D'EVENT
+
+    //CREER UN PROTOTYPE ET SA METHODE POUR RENPLIR LE VECTEUR associated_log AVEC L'ID DES LOGS DE L'ATTAQUE
+
+private:
+    int Id_Security_Event;
+    std::string Description;
+    EventGravity Event_Gravity;
+    std::vector<Logs*> Associated_Logs;
+    std::tm Time_Stamp;
+
+};
