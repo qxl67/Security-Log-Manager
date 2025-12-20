d12f91ed99053595cdefc0d36e6166621eb05601
tlecomte1 <lecomte.t@protonmail.com>
2025-12-20 01:56:18 +0100

    Update NewWindowsSystem_LogsTL.hpp

--- Projet/CodeTL/NewWindowsSystem_LogsTL.hpp
+++ Projet/CodeTL/NewWindowsSystem_LogsTL.hpp
@@ -21,7 +21,7 @@ protected:
 public:
 Windows_System_Logs(int id, 
     std::string synthesis_message,
-    std::time_T time_stamp, 
+    std::time_t time_stamp, 
     std::string source_log,
     std::string source_message, 
     int processus_id,
@@ -78,3 +78,4 @@ Windows_System_Logs(int id,
     //verifier si d'autre methodes sont necessaires a implanter
 
 };
+
